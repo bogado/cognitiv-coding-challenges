8051f8066f8471b5726168383cdac11b5d917b9c
Victor Bogado da Silva Lins <victor@bogado.net>
2021-10-23 12:56:45 -0700

    Fix the `binary_traits` for make it more conformant to the standard

--- dna/test/fake_stream.hpp
+++ dna/test/fake_stream.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstddef>
+#include <string>
 #include <string_view>
 #include <vector>
 #include <atomic>
@@ -9,7 +10,14 @@
 namespace detail
 {
 
-class binary_traits
+// XXX: I'm not sure that this class is adding any value.
+//
+// `to_upper`:: is not specified as part of a "char_traits" specification.
+// `length`:: This function is specified as returning the location of the first position where the value compares equal to the default constructed value (usually 0).
+//
+// see (http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2020/n4861.pdf#subsection.21.2.1)[char.traits.require]
+/// Character traits used on the `std::string` container on the `fake_stream` class.
+class binary_traits : public std::char_traits<std::byte>
 {
 public:
 	static constexpr std::byte to_upper(std::byte c) noexcept {
