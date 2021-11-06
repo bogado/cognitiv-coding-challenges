08daa36cdc2d57d322b68d6a4c572ff86ed7ec0d
Victor Bogado da Silva Lins <victor@bogado.net>
2021-11-06 13:48:36 -0700

    Update for the `Location` definition
    Makes the `Location` be a simple pointer and creates
    a more generic location type `InterestingLocation` for
    locating the interesting objects.

--- dna/person.hpp
+++ dna/person.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
+#include "base.hpp"
 #include "sequence_buffer.hpp"
+#include "interesting.hpp"
 
-#include <string>
+#include <cstddef>
 #include <iostream>
+#include <iterator>
 #include <sstream>
+#include <string>
 #include <ranges>
 
 namespace dna
@@ -17,49 +21,40 @@ concept HelixStream = requires(T a) {
 	{ a.size() } -> std::convertible_to<std::size_t>;
 };
 
-template<typename T>
-concept Person = requires(T a) {
-	{ a.chromosome(1) } -> HelixStream;
-	{ a.chromosomes() } -> std::convertible_to<std::size_t>;
-};
+struct Location {
+    std::size_t pos = 0;
 
-struct location {
-	std::size_t chromosome;
-	std::size_t position;
+    Location(std::bidirectional_iterator auto start, std::bidirectional_iterator auto pos)
+        : pos { static_cast<std::size_t>(std::distance(start, pos)) }
+    { }
 
-	friend std::ostream& operator <<(std::ostream& out, const location& me)
+    friend std::ostream& operator<<(std::ostream& out, Location loc)
 	{
-		return out << me.chromosome << ":" << me.position;
+        return out << '[' << loc.pos << ']';
 	}
 };
 
-template <typename MUTATION_TYPE>
-concept mutation_type = requires(const MUTATION_TYPE mutation, std::ostream& out) {
-	{ mutation.id() } -> std::convertible_to<std::string>;
-	{ out << mutation };
-	{ mutation.chomossome_location() } -> std::same_as<location>;
-	{ mutation.size() } -> std::same_as<std::size_t>;
+struct InterestingLocation {
+	static constexpr int NOT_DEFINED = -1;
+
+	all_interesting_t what;
+	Location loc;
+	int chromosome_num = NOT_DEFINED;
 };
 
-struct snp {
-	location loc;
-	base expected;
-	base found;
-
-	location chomossome_location() {
-		return loc;
+inline std::ostream& operator<<(std::ostream& out, const InterestingLocation& interesting)
+{
+	if (interesting.chromosome_num != InterestingLocation::NOT_DEFINED) {
+		out << interesting.chromosome_num << ":";
 	}
+	return out << interesting.loc << ": " << interesting.what;
+}
 
-	std::string id() {
-		auto out = std::stringstream{};
-		out << *this;
-		return out.str();
-	}
-
-	friend std::ostream& operator <<(std::ostream& out, const snp& me)
-	{
-		return out << me.loc << " Alleles: " << me.found << ">" << me.expected;
-	}
+template <typename T>
+concept Person = requires(T a)
+{
+    { a.chromosome(1) } -> HelixStream;
+    { a.chromosomes() } -> std::convertible_to<std::size_t>; 
 };
 
 }
