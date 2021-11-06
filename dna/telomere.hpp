0ce1d98daf04e8dd574b8b6e052d30f1e8616395
Victor Bogado da Silva Lins <victor@bogado.net>
2021-11-06 13:52:09 -0700

    Algorithm for skipping telomeres at the start of a sequence.

--- dna/telomere.hpp
+++ dna/telomere.hpp
@@ -2,8 +2,11 @@
 #define TELOMERE_HPP_INCLUDED
 
 #include "base.hpp"
+#include "sequence_buffer.hpp"
 
+#include <algorithm>
 #include <array>
+#include <ranges>
 
 namespace dna {
 
@@ -17,6 +20,51 @@ static constexpr auto Telomere = std::array {
 };
 
 constexpr auto TelomereSize = std::size(Telomere);
+
+constexpr auto skip_telomere(is_sequence_view auto& sequence) noexcept
+{
+    const auto empty_result = std::tuple {
+		std::ranges::subrange(std::begin(sequence), std::begin(sequence)),
+		std::ranges::subrange(std::begin(sequence), std::end(sequence))
+	};
+    if (std::ranges::size(sequence) <= 2) {
+        return empty_result;
+    }
+
+    auto start       = std::begin(sequence);
+    auto finish      = std::end(sequence);
+    auto telomere_it = std::begin(Telomere);
+
+    auto initial = *start;
+    switch (initial) {
+    case dna::A:
+        std::advance(telomere_it, 2);
+        break;
+    case dna::T:
+        if (*std::next(start) == dna::A) {
+            std::advance(telomere_it, 1);
+        }
+        break;
+    case dna::G: {
+        auto count = std::ranges::count(sequence | std::views::take(3), dna::G);
+
+        std::advance(telomere_it, 3 - count);
+    } break;
+    case dna::C:
+        return empty_result;
+    }
+
+    auto result = start;
+    while (start != finish && *telomere_it == *start) {
+        start++;
+        telomere_it++;
+        if (telomere_it == std::end(Telomere)) {
+            result      = start;
+            telomere_it = std::begin(Telomere);
+        }
+    }
+    return std::tuple { std::ranges::subrange(std::begin(sequence), result), std::ranges::subrange(result, std::end(sequence)) };
+}
 
 }
 
